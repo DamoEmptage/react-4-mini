2b77615e50845f09858b44972bc50644d61f8111
Damon Emptage <dadbodtv@gmail.com>
2019-02-14 20:20:24 -0700

    routing

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import { HashRouter, BrowserRouter, Route, Switch, Link, Redirect } from 'react-router-dom'
+import './App.css';
+
+import Route1 from './components/Route1';
+import Route2 from './components/Route2';
+import Route3 from './components/route3';
+import NotFound from './components/NotFound';
+
+
+
+class App extends Component {
+  render() {
+
+    return (
+      <HashRouter>
+        <div className="App">
+          <header className="App-header">
+            <nav>
+              <Link to='/1'>Home</Link>
+              <Link to='/3'>Profile</Link>
+              <Link to='/3'>Logout</Link>
+
+            </nav>
+            <img src={logo} className="App-logo" alt="logo" />
+            <Switch>
+              <Route path='/1' component={Route1} />
+              <Route path='/2' component={Route2} />
+              <Route path='/3' component={Route3} />
+              <Route path='/NotFound' component={NotFound} />
+              <Redirect to='/NotFound' />
+
+              <Route1 />
+              <Route2 />
+              <Route3 />
+            </Switch>
+
+          </header>
+        </div>
+      </HashRouter>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Route1.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link, Route, Switch } from 'react-router-dom';
+
+function Route1(props) {
+    return (
+        <h2>Route 1</h2>
+    );
+}
+
+export default Route1;
